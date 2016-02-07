437956fdadff0664b2217fbc5719b87bc92a2214
Oleg Natalushko <onatalushko@adyax.com>
2016-02-07 12:26:08 +0100

    Drupal.Axax undefined fixes

--- assets/vendor/jcarousel/jcarousel.js
+++ assets/vendor/jcarousel/jcarousel.js
@@ -158,7 +158,7 @@
   };
 
   /**
-   * Init callback for jCarousel. Pauses the carousel when hovering over.
+   * Auto pause callback for jCarousel. Pauses the carousel when hovering over.
    */
   Drupal.jcarousel.autoPauseCallback = function (event, carousel) {
     function pauseAuto() {
@@ -172,108 +172,113 @@
     }
   };
 
-  /**
-   * Sets the throbber progress indicator.
-   */
-  Drupal.Ajax.prototype.setProgressIndicatorJcarousel = function () {
-    console.log('progress');
-    console.log(this);
-    var jCarousel = $(this.element_settings.selector).find('[data-jcarousel]');
-    this.progress.element = $('<li><div class="ajax-progress ajax-progress-throbber ajax-progress-jcarousel"><div class="throbber">&nbsp;</div></div></li>');
-    jCarousel.jcarousel('list').append(this.progress.element);
-    jCarousel.jcarousel('reload');
-    console.log(jCarousel.jcarousel('items').size());
-    if (this.progress.message) {
-      this.progress.element.find('.throbber').after('<div class="message">' + this.progress.message + '</div>');
-    }
-    $(this.element).after(this.progress.element);
-  };
+  if (typeof Drupal.Ajax != "undefined") {
+    /**
+     * Sets the throbber progress indicator.
+     */
+    Drupal.Ajax.prototype.setProgressIndicatorJcarousel = function () {
+      console.log('progress');
+      console.log(this);
+      var jCarousel = $(this.element_settings.selector).find('[data-jcarousel]');
+      this.progress.element = $('<li><div class="ajax-progress ajax-progress-throbber ajax-progress-jcarousel"><div class="throbber">&nbsp;</div></div></li>');
+      jCarousel.jcarousel('list').append(this.progress.element);
+      jCarousel.jcarousel('reload');
+      console.log(jCarousel.jcarousel('items').size());
+      if (this.progress.message) {
+        this.progress.element.find('.throbber').after('<div class="message">' + this.progress.message + '</div>');
+      }
+      $(this.element).after(this.progress.element);
+    };
+  }
 
-  /**
-   * Command to insert new jCarousel slide into the DOM.
-   *
-   * @param {Drupal.Ajax} ajax
-   * @param {object} response
-   * @param {string} response.data
-   * @param {string} [response.method]
-   * @param {string} [response.selector]
-   * @param {object} [response.settings]
-   * @param {number} [status]
-   */
-  Drupal.AjaxCommands.prototype.jcarousel_append = function(ajax, response, status) {
-    // Get information from the response. If it is not there, default to
-    // our presets.
-    var wrapper = response.selector ? $(response.selector + ' .jcarousel-wrapper') : $(ajax.wrapper + ' .jcarousel-wrapper');
-    var slide_parent = wrapper.find('ul');
-    var jCarousel = wrapper.find('[data-jcarousel]');
-    var jCarouselOptions = jCarousel.jcarousel('options');
+  if (typeof Drupal.AjaxCommands != "undefined") {
+    /**
+     * Command to insert new jCarousel slide into the DOM.
+     *
+     * @param {Drupal.Ajax} ajax
+     * @param {object} response
+     * @param {string} response.data
+     * @param {string} [response.method]
+     * @param {string} [response.selector]
+     * @param {object} [response.settings]
+     * @param {number} [status]
+     */
+    Drupal.AjaxCommands.prototype.jcarousel_append = function (ajax, response, status) {
+      // Get information from the response. If it is not there, default to
+      // our presets.
+      var wrapper = response.selector ? $(response.selector + ' .jcarousel-wrapper') : $(ajax.wrapper + ' .jcarousel-wrapper');
+      var slide_parent = wrapper.find('ul');
+      var jCarousel = wrapper.find('[data-jcarousel]');
+      var jCarouselOptions = jCarousel.jcarousel('options');
 //    var control = $(ajax.element);
 //    var control_options = control.jcarouselControl('options');
-    var method = response.method || ajax.method;
-    var effect = ajax.getEffect(response);
-    var settings;
+      var method = response.method || ajax.method;
+      var effect = ajax.getEffect(response);
+      var settings;
 
-    // jCarousel slide should be wrapped with $('<li></li>').
-    var new_content_wrapped = $('<li></li>').html(response.data);
-    var new_content = new_content_wrapped.contents();
+      // jCarousel slide should be wrapped with $('<li></li>').
+      var new_content_wrapped = $('<li></li>').html(response.data);
+      var new_content = new_content_wrapped.contents();
 
-    // For legacy reasons, the effects processing code assumes that
-    // new_content consists of a single top-level element. Also, it has not
-    // been sufficiently tested whether attachBehaviors() can be successfully
-    // called with a context object that includes top-level text nodes.
-    // However, to give developers full control of the HTML appearing in the
-    // page, and to enable Ajax content to be inserted in places where DIV
-    // elements are not allowed (e.g., within TABLE, TR, and SPAN parents),
-    // we check if the new content satisfies the requirement of a single
-    // top-level element, and only use the container DIV created above when
-    // it doesn't. For more information, please see
-    // https://www.drupal.org/node/736066.
-    if (new_content.length !== 1 || new_content.get(0).nodeType !== 1) {
-      new_content = new_content_wrapped;
-    }
+      // For legacy reasons, the effects processing code assumes that
+      // new_content consists of a single top-level element. Also, it has not
+      // been sufficiently tested whether attachBehaviors() can be successfully
+      // called with a context object that includes top-level text nodes.
+      // However, to give developers full control of the HTML appearing in the
+      // page, and to enable Ajax content to be inserted in places where DIV
+      // elements are not allowed (e.g., within TABLE, TR, and SPAN parents),
+      // we check if the new content satisfies the requirement of a single
+      // top-level element, and only use the container DIV created above when
+      // it doesn't. For more information, please see
+      // https://www.drupal.org/node/736066.
+      if (new_content.length !== 1 || new_content.get(0).nodeType !== 1) {
+        new_content = new_content_wrapped;
+      }
 
-    // Add the new content to the page.
-    slide_parent[method](new_content);
+      // Add the new content to the page.
+      slide_parent[method](new_content);
 
-    // Immediately hide the new content if we're using any effects.
-    if (effect.showEffect !== 'show') {
-      new_content.hide();
-    }
+      // Immediately hide the new content if we're using any effects.
+      if (effect.showEffect !== 'show') {
+        new_content.hide();
+      }
 
-    // Determine which effect to use and what content will receive the
-    // effect, then show the new content.
-    if (new_content.find('.ajax-new-content').length > 0) {
-      new_content.find('.ajax-new-content').hide();
-      new_content.show();
-      new_content.find('.ajax-new-content')[effect.showEffect](effect.showSpeed);
-    }
-    else if (effect.showEffect !== 'show') {
-      new_content[effect.showEffect](effect.showSpeed);
-    }
+      // Determine which effect to use and what content will receive the
+      // effect, then show the new content.
+      if (new_content.find('.ajax-new-content').length > 0) {
+        new_content.find('.ajax-new-content').hide();
+        new_content.show();
+        new_content.find('.ajax-new-content')[effect.showEffect](effect.showSpeed);
+      }
+      else
+        if (effect.showEffect !== 'show') {
+          new_content[effect.showEffect](effect.showSpeed);
+        }
 
-    // Attach all JavaScript behaviors to the new content, if it was
-    // successfully added to the page, this if statement allows
-    // `#ajax['wrapper']` to be optional.
-    if (new_content.parents('html').length > 0) {
-      // Apply any settings from the returned JSON if available.
-      settings = response.settings || ajax.settings || drupalSettings;
-      Drupal.attachBehaviors(new_content.get(0), settings);
-    }
+      // Attach all JavaScript behaviors to the new content, if it was
+      // successfully added to the page, this if statement allows
+      // `#ajax['wrapper']` to be optional.
+      if (new_content.parents('html').length > 0) {
+        // Apply any settings from the returned JSON if available.
+        settings = response.settings || ajax.settings || drupalSettings;
+        Drupal.attachBehaviors(new_content.get(0), settings);
+      }
 
-    // Reload jCarousel and scroll it forward.
-    jCarousel.jcarousel('reload');
-    // @todo get scroll count from carousel settings.
-    jCarousel.jcarousel('scroll', '+=1');
+      // Reload jCarousel and scroll it forward.
+      jCarousel.jcarousel('reload');
+      // @todo get scroll count from carousel settings.
+      jCarousel.jcarousel('scroll', '+=1');
 
-    if (isNaN(ajax.element_settings.submit.page)) {
-      ajax.element_settings.submit.page = 1;
-    }
-    else {
-      if (response.settings.next_page == null) {
-        jCarousel.removeAttr('data-preload-page');
+      if (isNaN(ajax.element_settings.submit.page)) {
+        ajax.element_settings.submit.page = 1;
       }
       else {
-        ajax.element_settings.submit.page = response.settings.next_page;
+        if (response.settings.next_page == null) {
+          jCarousel.removeAttr('data-preload-page');
+        }
+        else {
+          ajax.element_settings.submit.page = response.settings.next_page;
+        }
       }
     }
   }
